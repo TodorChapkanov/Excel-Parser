0bacf49c99de5c4327a877b1ab2ef0d50d2de1fd
Todor <chapkanov.t@gmail.com>
2019-08-16 02:56:03 +0300

    Initial commit

---
+++ ExcelReader/DatabaseOperator.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReader
+{
+    public static class DatabaseOperator
+    {
+        private const string ConnectionString = "Server=.\\SQLEXPRESS;Database=ExcelReader;Trusted_Connection=True;MultipleActiveResultSets=true";
+      private const  string Excel03ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR={1}'";
+      private const  string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+      private const  string CSVConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='text;HDR=YES;FMT=Delimited'";
+       private const string TableName = "Product";
+        public static SqlConnection GetDbContext()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(ConnectionString);
+
+                connection.Open();
+
+                return connection;
+
+            }
+            catch (Exception es)
+
+            {
+                MessageBox.Show(es.Message);
+                return null;
+            }
+        }
+
+        public static void DisposeDbConnection(SqlConnection dbContext)
+        {
+            dbContext.Close();
+        }
+
+        public static ICollection<string> GetColumnNames()
+        {
+            var dbContext = GetDbContext();
+            string[] restrictions = new string[4] { null, null, "Product", null };
+
+            var columnList = dbContext.GetSchema("Columns", restrictions).AsEnumerable().Select(s => s.Field<String>("Column_Name")).ToList();
+            return columnList;
+        }
+
+        public static void ParsFile(string fullFilePath, SqlConnection dbContext)
+        {
+            string extension = Path.GetExtension(fullFilePath);
+            string conString = string.Empty;
+            switch (extension)
+            {
+                case ".xls":
+                   ParseExcel(fullFilePath, dbContext, string.Format(Excel03ConString, fullFilePath, "YES"));
+                    break;
+                case ".xlsx":
+                    ParseExcel(fullFilePath, dbContext,string.Format(Excel07ConString, fullFilePath, "YES"));
+                    break;
+                default:
+                    ParseCsv(fullFilePath);  // conString = string.Format(CSVConString, fullFilePath, "NO");
+                    break;
+            }
+        }
+
+        public static void ParseExcel(string fullFilePath, SqlConnection dbContext, string oleDbConnectionString)
+        {
+            try
+            {
+
+                OleDbConnection oledbconn = new OleDbConnection(oleDbConnectionString);
+
+                oledbconn.Open();
+                var schema = oledbconn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                var sheetName = schema.Rows[0]["Table_Name"].ToString();
+                ;
+
+                var dbColumnNameList = GetColumnNames();
+                var fileColumnNameList = new List<string>();
+                try
+                {
+                    var columns = oledbconn.GetOleDbSchemaTable(OleDbSchemaGuid.Columns, null);
+                  
+                    if (columns != null)
+                    {
+                        fileColumnNameList.AddRange(from DataRow column in columns.Rows select column["Column_name"].ToString());
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                var mappingColumnNames = new Dictionary<string, string>();
+                foreach (var name in dbColumnNameList)
+                {
+                    var mapp = fileColumnNameList.FirstOrDefault(f => f == name);
+                    if (mapp != null)
+                    {
+                        mappingColumnNames.Add(name, mapp);
+                        fileColumnNameList.Remove(name);
+                    }
+
+                }
+
+                SqlBulkCopy bulkcopy = new SqlBulkCopy(ConnectionString);
+
+                foreach (var item in mappingColumnNames)
+                {
+                    bulkcopy.ColumnMappings.Add(item.Key, item.Value);
+                }
+
+                string exceldataquery = $"select {string.Join(",", mappingColumnNames.Values)} from [{sheetName}]";
+                OleDbCommand oledbcmd = new OleDbCommand(exceldataquery, oledbconn);
+                OleDbDataReader dr = oledbcmd.ExecuteReader();
+
+                bulkcopy.DestinationTableName = TableName;
+                bulkcopy.WriteToServer(dr);
+                bulkcopy.Close();
+                oledbconn.Close();
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Invalid Operation!!!");
+            }
+        }
+        public static void ParseCsv(string filename)
+        {
+            var lines = System.IO.File.ReadAllLines(filename);
+            if (lines.Count() == 0) return;
+            var columns = lines[0].Split(',');
+            var table = new DataTable();
+            var fileColumnNameList = new List<string>();
+            foreach (var c in columns)
+            {
+                table.Columns.Add(c);
+                fileColumnNameList.Add(c);
+            }
+
+
+            for (int i = 1; i < lines.Count() - 1; i++)
+            {
+                table.Rows.Add(lines[i].Split(','));
+            }
+            var dbColumnNameList = GetColumnNames();
+            var mappingColumnNames = new Dictionary<string, string>();
+            foreach (var name in dbColumnNameList)
+            {
+                var mapp = fileColumnNameList.FirstOrDefault(f => f == name);
+                if (mapp != null)
+                {
+                    mappingColumnNames.Add(name, mapp);
+                    fileColumnNameList.Remove(name);
+                }
+
+            }
+
+            mappingColumnNames.Keys.ToList().ForEach(key => dbColumnNameList.Remove(key));
+
+           
+            DataTable dtInsertRows = table;
+
+            using (SqlBulkCopy sbc = new SqlBulkCopy(ConnectionString, SqlBulkCopyOptions.KeepIdentity))
+            {
+                sbc.DestinationTableName = TableName;
+
+                if (dbColumnNameList.Count != 0)
+                {
+                    foreach (var name in dbColumnNameList)
+                    {
+                        if (fileColumnNameList.Contains(name))
+                        {
+                            var mappingFileColumnName = fileColumnNameList.First(columnName => columnName.Contains(name));
+                            mappingColumnNames.Add(name, mappingFileColumnName);
+                        }
+                    }
+                   
+                }
+             
+                sbc.BatchSize = table.Rows.Count;
+
+                foreach (var item in mappingColumnNames)
+                {
+                    sbc.ColumnMappings.Add(item.Key, item.Value);
+                }
+                sbc.WriteToServer(dtInsertRows);
+            }
+        }
+    }
+}
---
+++ ExcelReader/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReader
+{
+    public partial class Form1 : Form
+    {
+        private const string InvalidFilePathMessage = "Invalid File Path";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            var dbContext = DatabaseOperator.GetDbContext();
+
+            var filePath = this.FilePathTextBox.Text;
+
+           DatabaseOperator.GetColumnNames();
+
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                MessageBox.Show(InvalidFilePathMessage);
+                return;
+            }
+
+            DatabaseOperator.ParsFile(filePath,dbContext);
+
+
+            try
+            {
+                DatabaseOperator.DisposeDbConnection(dbContext);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+       
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var fileName = openFileDialog.FileName;
+                this.FilePathTextBox.Text = fileName;
+            }
+        }
+    }
+}
